4af087f21464973cadf0b483a4a614618df941d8
Sana Zahlan <sanazahlan@yahoo.com>
2022-05-07 22:15:09 -0700

    Add functionality to return price of medium pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,8 @@ Pizza.prototype.calculatePrice = function() {
   let price;
   if (this.size === "Small") {
     price = "$8";
+  } else if (this.size === "Medium") {
+      price = "$10";
   }
   return price;
 }
